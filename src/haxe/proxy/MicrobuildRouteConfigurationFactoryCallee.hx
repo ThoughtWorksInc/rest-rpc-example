1c75a4aeab0d98d69d11626235bfaf3b41f3e25b
Shao Zengwei <zengwei19861029@163.com>
2015-11-19 17:17:20 +0800

    remove the usless code.

--- src/haxe/proxy/MicrobuildRouteConfigurationFactoryCallee.hx
+++
@@ -1,6 +0,0 @@
-package proxy;
-
-@:build(com.thoughtworks.microbuilder.core.RouteConfigurationFactory.generateRouteConfigurationFactory([
-    "rpc.IUserRpc"
-]))
-class MicrobuildRouteConfigurationFactoryCallee {}
